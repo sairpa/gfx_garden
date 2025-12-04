4e2cfa5e3c15267b4e595856d877c22c04f989ee
tintu <pranav.ajay2000@gmail.com>
2025-12-04 21:19:10 +0530

    kaboom :/

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ./build/debug && cd build
-# Configure the project (tell CMake to use GCC explicitly)
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
-# Build the project
-cmake --build .
-
-./gs
