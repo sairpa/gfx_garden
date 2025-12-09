a1dd164a7705483b565a6d0e5016bb99b5e91cf1
tintu <pranav.ajay2000@gmail.com>
2025-12-09 22:56:05 +0530

    day 2
    - slight cleanup of code
    - actual color rendering done :D
    - added the func needed for doing input validation
    - overall addition of context comments :D

--- window.cpp
+++ window.cpp
@@ -1,26 +1,22 @@
 // A program to try to spawn a window using opengl :D
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
-#include<iostream>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+#include<window.h>
 
 int main(){
-    std::cout<< "Hello OpenGL :D\n";
-
+    // GLFW Library inits and configuration
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // Window init and config
     GLFWwindow* window = glfwCreateWindow(1280,720, "heya tintu!", NULL, NULL);
     if(window == NULL){
         std::cout<< "Failed to create GLFW window :(\n";
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window); // linking the created window object to the glfw states
 
     if( !gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
         std::cout<< "Failed to intialize GLAD\n";
@@ -31,12 +27,25 @@ int main(){
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while(!glfwWindowShouldClose(window)){
+        // Input handling- currently only focussing on the exit :|
+        processInput(window);
+
+        // Rendering part here :D
+        glClearColor(0.13f, 0.5f, 0.8f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
---
+++ window.h
@@ -0,0 +1,6 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
