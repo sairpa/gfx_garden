e4ef3b4a0c7cacd4c091a50604113d93e813b51e
tintu <pranav.ajay2000@gmail.com>
2025-12-09 22:38:17 +0530

    day 2
    - window achieved :D

--- window.cpp
+++ window.cpp
@@ -3,6 +3,8 @@
 #include<GLFW/glfw3.h>
 #include<iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main(){
     std::cout<< "Hello OpenGL :D\n";
 
@@ -12,5 +14,31 @@ int main(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    GLFWwindow* window = glfwCreateWindow(1280,720, "heya tintu!", NULL, NULL);
+    if(window == NULL){
+        std::cout<< "Failed to create GLFW window :(\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if( !gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
+        std::cout<< "Failed to intialize GLAD\n";
+        return -1;
+    }
+
+    glViewport(0,0,1280,720);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0,0,width, height);
 }
