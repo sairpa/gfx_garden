1207bd4b4f0eecbc49776d534e412a2f117e3ce0
tintu <pranav.ajay2000@gmail.com>
2025-12-11 22:15:54 +0530

    day 4
    - approach 1

--- trw/main.cpp
+++ trw/main.cpp
@@ -1,10 +1,23 @@
 #include "tgaimage.h"
+#include<cmath>
 
 constexpr TGAColor white = {255, 255, 255, 255}; // BGRA order!!!
 constexpr TGAColor green = {0, 255, 0, 255};
 constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor blue = {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
+
+
+// approach 1
+// simple parametric line using barycentric coordinates
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color){
+    for(float t=0; t< 1; t+=0.2){
+        int x = std::round(ax + (bx-ax)*t);
+        int y = std::round(ay + (by-ay)*t);
+        framebuffer.set(x,y,color);
+    }
+}
+
 
 int main(int argc, char** argv){
     constexpr int width = 64;
@@ -14,6 +27,11 @@ int main(int argc, char** argv){
     int ax = 7, ay = 3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(bx, by, cx, cy, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax,ay,white);
     framebuffer.set(bx,by,white);
