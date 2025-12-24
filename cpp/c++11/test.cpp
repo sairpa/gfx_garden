2feff0be027599dd6641f963c818056d222a9486
tintu <pranav.ajay2000@gmail.com>
2025-12-24 22:46:05 +0530

    day 7
    - adding some stuff to be done for the work on cpp11
    - trying to learn and unlearn harmful stuff I got now on cpp :/
    - ideally should be doing cpp11, cpp14, cpp17, cpp20 and finally cpp23

---
+++ cpp/c++11/test.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// My test code snippet collection to learn c11 standards
+// g++ -std=c++11 /home/tintu/dev/gfx_garden/cpp/c++11/test.cpp -o test; ./test
+
+int main(){
+
+    // Code for range based iterator
+    // {
+    //     vector<int> list = {1,2,3,4};
+
+    //     for(int i:list){
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //  lambda expressions
+    // {
+    //     auto res = [](int x){
+    //         return x*x;
+    //     };
+
+    //     cout << res(5) << endl;
+    //     cout << res(0) << endl;
+    // }
+
+    // Smart pointers
+    {
+        // we have an auto_ptr but is depreciated in c17 so not needed but essentially 
+        // it is a smart auto allocate and deallocate pointer architecture but is phased out due to better impls
+        
+    }
+
+    return 0;
+}
