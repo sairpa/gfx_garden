8a620964b39328682aafdd9eacc5b07dbb3dfb2e
tintu <pranav.ajay2000@gmail.com>
2026-01-12 22:04:19 +0530

    day 11

--- cpp/test11.cpp
+++ cpp/test11.cpp
@@ -53,7 +53,7 @@ int main(){
     // }
 
     // Smart pointers
-    {
+    // {
         // we have an auto_ptr but is depreciated in c17 so not needed but essentially 
         // it is a smart auto allocate and deallocate pointer architecture but is phased out due to better impls
         // instead now we have unique, shared and weak pointers- unique_ptr shared_ptr weak_ptr
@@ -65,14 +65,14 @@ int main(){
         // cout << (k == nullptr) << " ," << *p << endl;
 
 
-        // shared_ptr<int> l,l0(new int(13));
-        // l = l0;
-        // cout << *l << *l0 << l0.use_count() << endl;
+    //     shared_ptr<int> l,l0(new int(13));
+    //     l = l0;
+    //     cout << *l << *l0 << l0.use_count() << endl;
 
-        // weak_ptr<int> l1(l0);
-        // cout << *l0 << l1.use_count() << l0.use_count() << endl;
+    //     weak_ptr<int> l1(l0);
+    //     cout << *l0 << l1.use_count() << l0.use_count() << endl;
 
-    }
+    // }
 
     // Move semantics :D
     // helps move/transfer ownership of objects instead of redundantly copying
