6bbb9ed7e829aaeb1cc6d7da4b5e75f135deb50b
ajay <pranav.ajay2000@gmail.com>
2026-04-29 21:31:09 +0530

    milestone 1: #2.1

--- webgpu_vanguard/main.cpp
+++ webgpu_vanguard/main.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
 #include <webgpu/webgpu.h>
 
+// If using Dawn
+#define WEBGPU_BACKEND_DAWN
+
+// If using wgpu-native
+// #define WEBGPU_BACKEND_WGPU
+
+// If using emscripten
+// #define WEBGPU_BACKEND_EMSCRIPTEN
+
 int main(int, char**){
 
     std::cout<< "Web GPU fun starts here! :D\n";
 
-    
+    // Descriptor WGPU
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = nullptr;
+
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    // Check for instance creation
+    if (!instance){
+        std::cerr << "WebGPU intialisazation failed!\n";
+        return -1;
+    }
+
+    // WGPU instance simple pointer
+    std::cout << "WebGPU instance: " << instance << "\n";
 
     return 0;
 
