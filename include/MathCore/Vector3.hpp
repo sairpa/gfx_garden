cb0111c53af792219723ac0d8e92cedfe6dde046
tintu <pranav.ajay2000@gmail.com>
2026-07-10 15:08:51 +0530

    updated Vector3 class

--- include/MathCore/Vector3.hpp
+++ include/MathCore/Vector3.hpp
@@ -1,1 +1,12 @@
 #pragma once
+#include<concepts>
+
+namespace MathCore{
+
+    template<isDecimalNumber T> struct Vector3{
+        T x,y,z;
+        constexpr Vector3():x(0), y(0), z(0) {}
+        constexpr Vector3(T x, T y, T z):x(x), y(y), z(z) {}
+    };
+
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
 #include"MathCore/Concepts.hpp"
+#include"MathCore/Vector3.hpp"
 
 template<MathCore::isDecimalNumber T>
 struct TestStruct{
     T value;
 };
 
+struct TestVector3{
+    MathCore::Vector3<float> vec;
+};
+
 int main(){
     std::cout << "Vanguard math library init!\n";
     TestStruct<float> test{1.23};
     std::cout << "Float test passed: " << test.value << std::endl;
+    TestVector3 testVec{MathCore::Vector3<float>()};
+    std::cout << "Vector3 test passed: " << testVec.vec.x << ", " << testVec.vec.y << ", " << testVec.vec.z << " \n";
+    testVec.vec = MathCore::Vector3<float>(1.f,2.f,3.f);
+    std::cout << "Modified vector3 test passed: " << testVec.vec.x << ", " << testVec.vec.y << ", " << testVec.vec.z << " \n";
     return 0;
 }
