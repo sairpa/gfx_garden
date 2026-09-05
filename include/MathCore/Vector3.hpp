b96c17e03055382a23f3be7bcf26e9b16da75272
tintu <pranav.ajay2000@gmail.com>
2026-09-05 22:38:01 +0530

    Updated Vector 3 Implements

--- include/MathCore/Vector3.hpp
+++ include/MathCore/Vector3.hpp
@@ -7,6 +7,32 @@ namespace MathCore{
         T x,y,z;
         constexpr Vector3():x(0), y(0), z(0) {}
         constexpr Vector3(T x, T y, T z):x(x), y(y), z(z) {}
+
+        // Operations 
+
+        constexpr Vector3<T> operator+(const Vector3<T>& i) const{
+            return Vector3<T>(x+i.x, y+i.y, z+i.z);
+        }
+
+        constexpr Vector3<T> operator-(const Vector3<T>& i)const{
+            return Vector3<T>(x-i.x, y-i.y, z-i.z);
+        }
+
+        // Scalar Arg and Scalar Return
+        constexpr T dot(const Vector3& i) const{
+            return (x*i.x) + (y*i.y) + (z*i.z);
+        }
+
+        // Cross Product - Vector Return
+        // Surface Normal math
+        constexpr Vector3<T> cross(const Vector3& i) const{
+            return Vector3<T>(
+                (y*i.z) - (z*i.y), 
+                (z*i.x) - (x*i.z), 
+                (x*i.y) - (y*i.x)
+            );
+        }
+
     };
 
 }
--- test/test.cpp
+++ test/test.cpp
@@ -2,22 +2,24 @@
 #include"MathCore/Concepts.hpp"
 #include"MathCore/Vector3.hpp"
 
-template<MathCore::isDecimalNumber T>
-struct TestStruct{
-    T value;
-};
-
-struct TestVector3{
-    MathCore::Vector3<float> vec;
-};
 
 int main(){
     std::cout << "Vanguard math library init!\n";
-    TestStruct<float> test{1.23};
-    std::cout << "Float test passed: " << test.value << std::endl;
-    TestVector3 testVec{MathCore::Vector3<float>()};
-    std::cout << "Vector3 test passed: " << testVec.vec.x << ", " << testVec.vec.y << ", " << testVec.vec.z << " \n";
-    testVec.vec = MathCore::Vector3<float>(1.f,2.f,3.f);
-    std::cout << "Modified vector3 test passed: " << testVec.vec.x << ", " << testVec.vec.y << ", " << testVec.vec.z << " \n";
+    
+    MathCore::Vector3<float> v1(1.0f, 0.f, 0.f);
+    MathCore::Vector3<float> v2(0.f, 1.f, 0.f);
+    
+    MathCore::Vector3<float> addRes = v1 + v2;
+    std::cout << "Vector3 addition test passed: " << addRes.x << ", " << addRes.y << ", " << addRes.z << " \n";
+
+    MathCore::Vector3<float> subRes = v1 - v2;
+    std::cout << "Vector3 subtraction test passed: " << subRes.x << ", " << subRes.y << ", " << subRes.z << " \n";
+
+    MathCore::Vector3<float> crossRes = v1.cross(v2);
+    std::cout << "Vector3 cross product test passed: " << crossRes.x << ", " << crossRes.y << ", " << crossRes.z << " \n";
+
+    float dotRes = v1.dot(v2);
+    std::cout << "Vector3 dot product test passed: " << dotRes << " \n";
+
     return 0;
 }
